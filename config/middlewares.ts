27e718da5ee6a1e77697e62681885bfd58479379
Dima Markus <dmi3000@gmail.com>
2023-03-16 14:34:28 -0400

    PLUGIN - add AWS S3 plugin for media uploads

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,45 @@
-export default [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+export default ({ env }) => [
+  "strapi::errors",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+  // 'strapi::security',
+  // https://market.strapi.io/providers/@strapi-provider-upload-aws-s3#install-now-section
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            env(
+              "AWS_BUCKET_NAME",
+              "yourBucketName.s3.yourRegion.amazonaws.com"
+            ),
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            env(
+              "AWS_BUCKET_NAME",
+              "yourBucketName.s3.yourRegion.amazonaws.com"
+            ),
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
