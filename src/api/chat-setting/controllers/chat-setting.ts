06c56d961f22b133314d6c9ed3a55a13ed412211
Dima Markus <dmi3000@gmail.com>
2023-03-20 18:39:27 -0400

    SCHEMA - Add Chat app settings

---
+++ src/api/chat-setting/controllers/chat-setting.ts
@@ -0,0 +1,7 @@
+/**
+ * chat-setting controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::chat-setting.chat-setting');
---
+++ src/api/chat-setting/services/chat-setting.ts
@@ -0,0 +1,7 @@
+/**
+ * chat-setting service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::chat-setting.chat-setting');
